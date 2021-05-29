f8aeec853db0a1aa224bffac5671cedd8daad69b
dk731 <demid.kaidalov@gmail.com>
2021-05-29 17:23:05 +0300

    Added loggin page

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 const fs = require("fs");
+
+let db = new sqlite3.Database("./db/MAIN.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to the chinook database.");
+});
 
 const app = express();
 const port = 3000;
 
-const send_amount = 150;
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile("/main.html", { root: "views" });
@@ -14,10 +22,15 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.get("/public/js/index.js", (req, res) => {
-  res.sendFile("/js/index.js", { root: "public" });
+app.post("/submit_login", function (req, res) {
+  res.send("POST request to the homepage");
 });
 
-app.get("/public/css/main.css", (req, res) => {
-  res.sendFile("/css/main.css", { root: "public" });
+app.get("/login", (req, res) => {
+  res.sendFile("login.html", { root: "views" });
+});
+
+app.get("/postinf/", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(req.query));
 });
